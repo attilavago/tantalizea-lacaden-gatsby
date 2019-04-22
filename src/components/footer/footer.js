9cf0a129e807a5001c6c0a52df32bf065735ad9b
Attila Vago <attilavago85@gmail.com>
2019-04-22 18:27:37 +0100

    footer done and looks nice

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Button from '../button/button';
-import { FaInstagram, FaFacebook } from 'react-icons/fa';
+import { FaInstagram, FaFacebookF } from 'react-icons/fa';
+import Styles from './footer.module.scss';
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className={Styles.footer}>
             <nav>
                 <ul>
-                    <li><a href="https://www.instagram.com/sweetspero/" target="_blank"><FaInstagram/></a></li>
-                    <li><a href="https://www.facebook.com/tantalizea/" target="_blank"><FaFacebook/></a></li>
+                    <li><a className={Styles.styledIcon} href="https://www.instagram.com/sweetspero/" target="_blank"><FaInstagram/></a></li>
+                    <li><a className={Styles.styledIcon} href="https://www.facebook.com/tantalizea/" target="_blank"><FaFacebookF/></a></li>
                     <li><Link to="">terms of use</Link></li>
                     <li><Link to="">privacy policy</Link></li>
+                    <li><p className={Styles.copyright}>© 2019 Tantalizea Lacaden</p></li>
                 </ul>
             </nav>
-            <p>© 2019 Tantalizea Lacaden</p>
-            <section>
-                <p>don't leave without becoming a <span className="allCaps">vip</span>!</p>
+            <section className={Styles.callToAction}>
+                <p>Don't leave without becoming a <span className="allCaps">vip</span>!</p>
                 <Button buttonText="sign me up!"/>
             </section>
         </footer>
