4530c80c7bfd15963c845bedaf7d712758d795e0
Attila Vago <attilavago85@gmail.com>
2019-05-10 20:25:00 +0100

    404 page sorted

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Layout from '../components/layout/layout';
 import { Link } from 'gatsby';
+import Styles from './404.module.scss';
 
 const NotFound = () => {
     return (
         <Layout>
-            <h1>Page not found</h1>
-            <Link to="/">Head home</Link>
+            <section className={Styles.fourOhFour}>
+                <img role="presentation" src="../404.svg" alt=""/>
+                <p>We can't find the page you're looking for.</p>
+                <p>We could take you back to the <Link to="/">homepage</Link>.</p>
+            </section>
         </Layout>
     )
 }
