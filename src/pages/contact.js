eaa65e0362abdeb8ea33cf0512199070d3a9577d
Attila Vago <attilavago85@gmail.com>
2019-05-20 21:00:27 +0100

    calendly loads ok

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Layout from '../components/layout/layout.js';
 
-const ContactPage = () => {
-    return (
-        <Layout>
-            <h1>Contact</h1>
-            <p>Contact page content...</p>
-            <div className="calendly-inline-widget" data-url="https://calendly.com/tantalizea/design-consultation?hide_event_type_details=1"></div>
-            <script async type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
-        </Layout>
-    )
+class ContactPage extends React.Component {
+    componentDidMount() {
+        const head = document.querySelector('head');
+        const script = document.createElement('script');
+        script.setAttribute('src',  'https://assets.calendly.com/assets/external/widget.js');
+        head.appendChild(script);
+    }
+
+    render() {
+        return (
+            <Layout>
+                <h1>Contact</h1>
+                <p>Contact page content...</p>
+                <div 
+                    className="calendly-inline-widget"
+                    data-url="https://calendly.com/tantalizea/design-consultation?hide_event_type_details=1"
+                    style={{ minWidth: '320px', height: '580px' }} 
+                />
+            </Layout>
+        )
+    }
+    
+    
 }
 
 export default ContactPage;
