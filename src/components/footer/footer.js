3d5b6f89003de6d17cdf94199249ef00b9cc8399
Attila Vago <attilavago85@gmail.com>
2019-06-03 18:05:28 +0100

    mailchimp subscription forms works but unstyled

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -43,7 +43,30 @@ class Footer extends Component {
                             <div>
                                 <h3>Want to know how to attract new customers?</h3>
                                 <p>Sign up to receive your freebie on why it's important to update your website <strong><em>today</em></strong>.</p>
-                            </div>
+                                
+                                <div id="mc_embed_signup">
+                                <form action="https://tantalizea.us3.list-manage.com/subscribe/post?u=dc7cecd4f9a2466b9d458df44&amp;id=01e65d504a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+                                    <div id="mc_embed_signup_scroll">
+                                    
+                                <div className="mc-field-group">
+                                    <label htmlFor="mce-FNAME">First Name </label>
+                                    <input type="text" defaultValue="" name="FNAME" className="" id="mce-FNAME"/>
+                                </div>
+                                <div className="mc-field-group">
+                                    <label htmlFor="mce-EMAIL">Email Address </label>
+                                    <input type="email" defaultValue="" name="EMAIL" className="required email" id="mce-EMAIL"/>
+                                </div>
+                                    <div id="mce-responses" className="clear">
+                                        <div className="response" id="mce-error-response" style={{display: 'none'}}></div>
+                                        <div className="response" id="mce-success-response" style={{display: 'none'}}></div>
+                                    </div>
+                                    <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true"><input type="text" name="b_dc7cecd4f9a2466b9d458df44_01e65d504a" tabIndex="-1" defaultValue=""/></div>
+                                    <div className="clear"><input type="submit" defaultValue="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/></div>
+                                    </div>
+                                </form>
+                                </div>
+
+                                </div>
                         </Modal>
                     }
             </footer>
